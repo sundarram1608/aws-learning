c2aae7725abb631cc0f1b459dfc92e049bf4a4f4
Sundar Ram Subramanian <110481773+sundarram1608@users.noreply.github.com>
2026-03-12 11:08:25 -0700

    Add installation script for Python app setup

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -1,1 +1,14 @@
 #install_app.sh
+#!/bin/bash
+set -e
+
+APP_DIR="/home/ubuntu/aws-learning/test-app"
+
+cd "$APP_DIR"
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install streamlit
